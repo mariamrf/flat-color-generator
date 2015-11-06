eed9fc01e2a5983cb600111a15b11b1b519fcbb1
Ramy Tamer <ramytamer94@gmail.com>
2015-11-07 00:54:43 +0200

    Fixed form issue (return button issue), simple validation for hue value.

--- js/script.js
+++ js/script.js
@@ -11,24 +11,29 @@ function generate() {
 
 }
 
-document.getElementById("remove").addEventListener('click', removeAll);
 removeAll = function() {
 	$('.all').empty();
 	generate();
 };
 
+document.getElementById("remove").addEventListener('click', removeAll);
+
+
 document.getElementById("more").addEventListener('click', generate);
 document.getElementById("hueChange").addEventListener('click', generateSpecific);
 
 function generateSpecific() {
-	var hue = document.getElementById("newHue").value;
-	$('.all').empty();
-	$('.more').empty();
-	for (var i = 0; i < 12; i++) {
-		var hsv = flatColorfromHue(hue);
-		var rgb = HSVtoRGB(hsv.h, hsv.s, hsv.v);
-		var temp = RGBtoHEX(rgb.r, rgb.g, rgb.b);
-		var hex = temp.toUpperCase();
-		$('.all').append('<div id="colorBlock" class="color" style="background-color:rgb(' + rgb.r + "," + rgb.g + "," + rgb.b + ');"><span>' + 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')<br>' + hex + '</span></div>');
+	var hue = parseInt($('#newHue').val(), 10);
+	if (hue && hue > 0) {
+		$('.all').empty();
+		$('.more').empty();
+		for (var i = 0; i < 12; i++) {
+			var hsv = flatColorfromHue(hue);
+			var rgb = HSVtoRGB(hsv.h, hsv.s, hsv.v);
+			var temp = RGBtoHEX(rgb.r, rgb.g, rgb.b);
+			var hex = temp.toUpperCase();
+			$('.all').append('<div id="colorBlock" class="color" style="background-color:rgb(' + rgb.r + "," + rgb.g + "," + rgb.b + ');"><span>' + 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')<br>' + hex + '</span></div>');
+		}
 	}
+	$('#newHue').val('');
 }
