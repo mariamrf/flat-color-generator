4906cb0f5a7b8a72abe1e57b34192d6f10ca169f
Mariam M <mariam.mrf@hotmail.com>
2015-11-07 00:16:23 +0200

    initial commit

---
+++ js/flatcolor.js
@@ -0,0 +1,48 @@
+
+function flatColor(){ //return type: Object
+	var PHI = 0.618033988749895;
+	var h, s, v;
+	hue = (Math.floor(Math.random()*(360 - 0 + 1)+0))/360;
+	h = ( hue + ( hue / PHI )) % 360;                                             
+	v = Math.floor(Math.random() * (100 - 20 + 1) + 20);
+	s = (v-10)/100;
+	v = v/100;
+	
+	return {h: h, s: s, v: v};
+}
+
+function flatColorfromHue(h){
+	h = h/360;
+	var s, v;                                            
+	v = Math.floor(Math.random() * (100 - 20 + 1) + 20);
+	s = (v-10)/100;
+	v = v/100;
+	
+	return {h: h, s: s, v: v};
+}
+
+function HSVtoRGB(h, s, v){
+	var r, g, b, i, f, p, q, t;
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+        r = Math.round(r * 255);
+        g = Math.round(g * 255);
+        b = Math.round(b * 255);
+    return {r: r, g: g, b: b};
+}
+
+function RGBtoHEX(r, g, b){
+	 var finalColor = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    return finalColor;
+}
