17037121ab35abe8e958cc4c25dfc39f2dfd016a
Mariam M <mariam.mrf@hotmail.com>
2016-01-12 15:45:09 +0200

    validation for hue value

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(window).load(generate);
 
 function generate() {
+	$('#errorMessage').empty();
+	$('#newHue').val('');
 	for (var i = 0; i < 12; i++) {
 		var hsv = flatColor();
 		var rgb = HSVtoRGB(hsv.h, hsv.s, hsv.v);
@@ -17,16 +19,15 @@ removeAll = function() {
 };
 
 document.getElementById("remove").addEventListener('click', removeAll);
-
-
 document.getElementById("more").addEventListener('click', generate);
 document.getElementById("hueChange").addEventListener('click', generateSpecific);
 
 function generateSpecific() {
 	var hue = parseInt($('#newHue').val(), 10);
-	if (hue && hue > 0) {
+	if (hue && hue > 0 && hue < 360) {
 		$('.all').empty();
 		$('.more').empty();
+		$('#errorMessage').empty();
 		for (var i = 0; i < 12; i++) {
 			var hsv = flatColorfromHue(hue);
 			var rgb = HSVtoRGB(hsv.h, hsv.s, hsv.v);
@@ -35,5 +36,8 @@ function generateSpecific() {
 			$('.all').append('<div id="colorBlock" class="color" style="background-color:rgb(' + rgb.r + "," + rgb.g + "," + rgb.b + ');"><span>' + 'rgb(' + rgb.r + ',' + rgb.g + ',' + rgb.b + ')<br>' + hex + '</span></div>');
 		}
 	}
-	$('#newHue').val('');
+	else {
+		$('#errorMessage').empty().append('Value must be between 0 & 360!');
+		$('#newHue').val('');
+	}
 }
