21d354e23b4a19d47f749c42255ed6090556b138
Mariam <mariam.mrf@hotmail.com>
2016-07-02 19:25:06 +0200

    Change html

--- js/script.js
+++ js/script.js
@@ -1,50 +1,33 @@
-$(document).ready(function(){
+var isHue = false;
+
+$(function(){
 	generate();
+	$('#hue-form').submit(function(e){
+		e.preventDefault();
+		isHue = true;
+		$('#grid').empty();
+		generate($('#hue').val());
+	});
+	$('#refresh').click(function(e){
+		isHue = false;
+		$('#grid').empty();
+		generate();
+	});
+	$('#more').click(function(e){
+		if(isHue)
+			generate($('#hue').val());
+		else
+			generate();
+	});
 });
 
 function generate(h) {
-	$('#errorMessage').empty();
-	if(h===undefined){
-		$('#newHue').val('');
-	for (var i = 0; i < 12; i++) {
-		var color = new flatColor();
+	for(var i=0; i<55; i++){
+		var color = new flatColor(h);
 		var hex = color.hex.toUpperCase();
-		$('.all').append('<div id="colorBlock" class="color" style="background-color:rgb(' + color.r + "," + color.g + "," + color.b + ');"><span>' + 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')<br>' + hex + '</span></div>');
+		var textClass = 1 - ( 0.299 * color.r + 0.587 * color.g + 0.114 * color.b)/255 < 0.5 ? 'dark-text' : 'light-text';
+		$('#grid').append('<div id="color-'+ i +'-'+hex+'" class="color colorBlock '+textClass+'" style="background-color:rgb(' + color.r + "," + color.g + "," + color.b + ');"><span>' + 'rgb(' + color.r + ',' + color.g + ',' + color.b + ')<br>' + hex + '</span></div>');
 	}
 }
-	else{
-		for (var i = 0; i < 12; i++) {
-			var col = new flatColor(h);
-			var hex = col.hex.toUpperCase();
-			$('.all').append('<div id="colorBlock" class="color" style="background-color:rgb(' + col.r + "," + col.g + "," + col.b + ');"><span>' + 'rgb(' + col.r + ',' + col.g + ',' + col.b + ')<br>' + hex + '</span></div>');
-		}
-	}
 
-
-}
-
-removeAll = function() {
-	$('.all').empty();
-	generate();
-};
-
-document.getElementById("remove").addEventListener('click', removeAll);
-document.getElementById("more").addEventListener('click', function(){
-	if($('#newHue').val()==='')
-		generate();
-	else
-		generate(parseInt($('#newHue').val(), 10));
-});
-document.getElementById("hueChange").addEventListener('click', function(){
-	var hue = parseInt($('#newHue').val(), 10);
-	if (hue && hue >= 0 && hue <= 360) {
-		$('.all').empty();
-		generate(hue);
-	}
-	else{
-		$('#errorMessage').empty().append('Value must be between 0 & 360!');
-		$('#newHue').val('');
-	}
-
-});
 
